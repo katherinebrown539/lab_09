b777d3efb3a13c16cc5156a4a92bf92c4bdd7663
Katherine Brown <katherine.e.brown.68@gmail.com>
2015-03-25 19:48:58 -0500

    Okay, I believe the lab is finished. Please test and let me know if there are any probz. --KEB

--- Sort.h
+++ Sort.h
@@ -1,3 +1,12 @@
+/*
+	lab_09
+	CSC 2111 Lab 09
+	Katherine (Katie) Brown and Amber Patterson
+	Note: A second makefile is used in build.bat to create an executable to test the quicksort individually
+	https://github.com/kebrown46/lab_09
+*/
+
+
 #if !defined (SORT_H)
 #define SORT_H
 
